646f32e90569e4fc06b69c2b4a632a8e9b8f6b96
Arya <aryaduta193@gmail.com>
2024-10-16 22:52:43 +0700

    type data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arya")
+	fmt.Println("Arya Duta")
+	fmt.Println("Arya Duta Perdana")
+
+	fmt.Println(len("Arya")) // Jumlah karakter string
+	fmt.Println("Arya Duta"[0]) // Ambil karakter pada posisi yg ditentukan. Output dalam byte
+	fmt.Println("Arya Duta Perdana"[1]) // Output dalam byte
+}
