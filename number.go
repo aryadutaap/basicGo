256449c46942562c9eeae63f00cb1aa7baa28265
Arya <aryaduta193@gmail.com>
2024-10-16 22:27:09 +0700

    type data number

---
+++ number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() { // Nama function tidak boleh sama. Nama main sudah ada pada file helloworld.go
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma lima = ", 3.5)
+}
